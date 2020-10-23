53f0a0d47fdf6c34dbf34a4786318818c01c3fd6
Ramakant849 <69476669+Ramakant849@users.noreply.github.com>
2020-10-23 14:22:46 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2@+!@bi&9%*qi%wun)vjwg$m(37b1lxg5fm^gc-+*qpdsj1-t#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','zzzzzz1.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','zzzzzz1.pythonanywhere.com']
 
 
 # Application definition
